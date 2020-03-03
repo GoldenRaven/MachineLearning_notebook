aee78e038076f8e016b6e882072334c8f091c5fd
ligy <ligy-_-@outlook.com>
2020-03-03 13:42:18 +0800

    test

--- crop-convert.bash
+++ crop-convert.bash
@@ -4,7 +4,7 @@ cd pdfs/
 pwd
 for file in *.pdf
 do
-    pdfcrop --margins '40 0 40 0' $file 1.pdf
+    pdfcrop --margins '60 0 40 0' $file 1.pdf
     convert -density 200 1.pdf -quality 90 1.png
     s=`echo ${file} |cut -d '.' -f 1`
     mv 1.png ../images/$s.png
