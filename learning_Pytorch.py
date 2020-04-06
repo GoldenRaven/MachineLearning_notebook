359ec37ed3785bf1f65e74633ea45c8e970903c3
GoldenRaven <ligy-_-@outlook.com>
2020-04-06 15:40:12 +0800

    feat: add csv to dict

--- learning_Pytorch.py
+++ learning_Pytorch.py
@@ -45,7 +45,11 @@ transform = transforms.Compose([transforms.ToTensor(),
                             transforms.Normalize((0.5,), (0.5,))])
 trainset = datasets.MNIST('../test-me/data', download=False,
                           train=True, transform=transform)
+testset = datasets.MNIST('../test-me/data', download=False,
+                          train=False, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
+                                          shuffle=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64,
                                           shuffle=True)
 dataiter = iter(trainloader)
 images, label = dataiter.next()
@@ -118,3 +122,80 @@ class Net(nn.Module):
         x = F.softmax(self.output, dim=1)
         return x
 # 11.PyTorch中的网络架构
+
+model = nn.Sequential(nn.Linear(784, 128),
+                      nn.ReLU(),
+                      nn.Linear(128, 64),
+                      nn.ReLU(),
+                      nn.Linear(64, 10),
+                      nn.LogSoftmax(dim=1))
+criterion = nn.CrossEntropyLoss()
+dataiter = iter(trainloader)
+images, label = dataiter.next()
+images = images.view(images.shape[0], -1)
+y_pred = model(images)
+loss = criterion(y_pred, label)
+print(loss)
+
+import torch.nn.functional as F
+criterion = nn.NLLLoss()
+dataiter = iter(trainloader)
+images, label = dataiter.next()
+images = images.view(images.shape[0], -1)
+y_pred = model(images)
+loss = criterion(y_pred, label)
+print(loss)
+
+# autograd
+x = torch.randn(2,2, requires_grad=True)
+y = x**2
+z = y.mean()
+print(y.grad_fn, z)
+z.backward()
+print(x.grad)
+print(x/2)
+
+from torch import optim
+optimizer = optim.SGD(model.parameters(), lr=0.003)
+epochs = 30
+f_out = open('loss.csv', 'w')
+for e in range(epochs):
+    train_loss, test_loss = [], []
+    running_loss, testing_loss = 0, 0
+    for images, label in trainloader:
+        images = images.view(images.shape[0], -1)
+        optimizer.zero_grad()
+        y_pred = model.forward(images)
+        loss = criterion(y_pred, label)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    else:
+        accuracy = 0
+        with torch.no_grad():
+            for images, label in testloader:
+                images = images.view(images.shape[0], -1)
+                y_pred = model.forward(images)
+                loss = criterion(y_pred, label)
+                testing_loss += loss.item()
+                ps = torch.exp(y_pred) # get probabilities
+                top_p, top_class = ps.topk(1, dim=1)
+                # print(top_class[:5, :], )
+                equals = top_class == label.view(top_class.shape)
+                accuracy += torch.mean(equals.type(torch.FloatTensor))
+        train_loss.append(running_loss/len(trainloader))
+        test_loss.append(testing_loss/len(testloader))
+        print(f"Epoch: {e+1}/{epochs}")
+        print(f"Training loss: {running_loss/len(trainloader)}")
+        print(f"Testing  loss: {testing_loss/len(testloader)}")
+        print(f"Accuracy: {accuracy.item()/len(trainloader)*100}%")
+        f_out.write(str(running_loss/len(trainloader))+' '+str(testing_loss/len(testloader))+'\n')
+f_out.close()
+import matplotlib.pyplot as plt
+plt.figure()
+plt.plot(train_loss, label='train loss')
+plt.plot(test_loss, label='test_loss')
+plt.legend()
+plt.savefig('train-test_loss.pdf')
+
+# 如何处理过拟合？drop
